9db8fe583b17f854465edce8c6b94eef3b77a7c9
hyun7494 <et009153@gmail.com>
2022-08-23 01:03:55 +0900

    2022/08/23 01:03 집 수정

--- Baekjoon/src/step01/_10.java
+++ Baekjoon/src/step01/_10.java
@@ -1,13 +1,28 @@
 package step01;
+/*
+ * 인터넷 봤음
+ * 날짜 : 2022/08/23
+ * 이름 : 김현준
+ * 내용 : 입력받고 사칙연산
+ * 
+ */
 
 import java.util.Scanner;
 
 public class _10 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int b = 1, 1, 2, 2, 2, 8;
-		int a = sc.nextInt();
-		
-	}
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int chess[] = {1, 1, 2, 2, 2, 8};
+        int findChess[] = new int[chess.length];
+
+        for(int i =0; i< chess.length; i++){
+            findChess[i] = sc.nextInt();
+        }
+
+        for(int i =0; i < chess.length; i++){
+            System.out.printf("%d ", chess[i]- findChess[i] );
+        }
+    }
 }
