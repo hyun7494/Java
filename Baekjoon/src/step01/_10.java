79db036217e340ce6e4598181d6f9967dc8f32d7
et009153 <et009153@gmail.com>
2022-08-22 13:56:11 +0900

    2022/08/22 13:56 학원 수정

--- Baekjoon/src/step01/_10.java
+++ Baekjoon/src/step01/_10.java
@@ -1,5 +1,13 @@
 package step01;
 
+import java.util.Scanner;
+
 public class _10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int b = 1, 1, 2, 2, 2, 8;
+		int a = sc.nextInt();
+		
+	}
 
 }
