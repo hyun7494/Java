619589488964942eeda0cac6f1538dab9ca348ee
et009153 <et009153@gmail.com>
2022-08-31 13:54:57 +0900

    2022/08/31 13:54 학원수정

---
+++ Baekjoon/src/step03/_10.java
@@ -0,0 +1,20 @@
+package step03;
+
+import java.util.Scanner;
+
+public class _10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		int [] A= new int[N];
+		
+		for(int i = 1; i<=N; i++) {
+			A[i-1] = sc.nextInt();
+			 
+		}
+	}
+
+}
