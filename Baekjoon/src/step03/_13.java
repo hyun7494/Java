b73edd970b6c6815aaa75243a19139f9d7a0b209
et009153 <et009153@gmail.com>
2022-09-02 13:54:49 +0900

    2022/09/02 13:54 학원 수정

--- Baekjoon/src/step03/_13.java
+++ Baekjoon/src/step03/_13.java
@@ -1,7 +1,16 @@
 package step03;
 
+import java.util.Scanner;
+
 public class _13 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N[] = sc.nextInt();
+		N = new int[1];
+		
+		if(int N[0] = empty ) {
+			int N[0] = 0;
+		}// 만약 N이 10
 		
 	}
 
