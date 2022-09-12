e8848793ee50248cd8a77f0c3ec6bd5e555a48eb
hyun7494 <et009153@gmail.com>
2022-09-12 23:34:01 +0900

    2022/09/12 23:33 집 수정

---
+++ Exercise/src/sub1/Q1.java
@@ -0,0 +1,27 @@
+package sub1;
+
+import java.util.Scanner;
+
+public class Q1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(); // 입력받을 임의의 자연수
+		int i;	
+		int j;
+		
+		for(i=2; i<=a; i++) {
+			
+			for(j=2; j<=i; j++) {
+				
+				if(i % j == 0)
+					break;
+			}
+			
+			if(i == j)
+				System.out.println(i);
+		}
+	}
+
+}
