3f049caa00b224a58c87f91650c356590c687d22
et009153 <et009153@gmail.com>
2022-09-26 13:55:41 +0900

    2022/09/26 13:55 학원수정

---
+++ Exam/src/sub06/Test09.java
@@ -0,0 +1,25 @@
+package sub06;
+
+public class Test09 {
+	
+	public static void main(String[] args) {
+		pyramid(3);
+		pyramid(5);
+		pyramid(7);
+		
+	}
+	public static void pyramid(int n) {
+		
+		for(int i = 1; i <= n; i++) { // 층
+			
+			for(int j = i; j < n; j++) // 빈칸 갯수
+				System.out.print(" ");
+			
+			for(int k = 1; k <= (2*i)-1 ; k++)
+				System.out.print("*");
+			
+			System.out.print("\n");
+		}
+	}
+
+}
